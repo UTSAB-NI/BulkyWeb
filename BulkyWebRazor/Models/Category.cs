96d7b43f0b703a23d5b7b7114596c634ac153a8b
Mjolnir <utsabsingh5@gmail.com>
2024-02-08 21:17:30 +0545

    phase one done

---
+++ BulkyWebRazor/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWebRazor.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name should be supplied")]
+        [DisplayName("Category Name")]
+        [MaxLength(30, ErrorMessage = "Category Name should be less than 30 character")]
+        public string? Name { get; set; }
+        [Range(0, 50, ErrorMessage = "Display Order should be between 0 and 50")]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
